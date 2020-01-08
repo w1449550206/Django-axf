a72ca9e7713bfb60bd24b4e4665216a45399b97c
w1449550206 <1449550206@qq.com>
2020-01-08 20:35:53 +0800

    1.修改页面架构2.完成statics的main的架构

--- axf/settings.py
+++ axf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e(*6n!rf_@uwwe2!9d!#l#3mh0sjfl!9t6yx8v^x1#1*&&q^ok'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
