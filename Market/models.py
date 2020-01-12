066dfab72c782c994b031789dab6f65d4ab58d72
w1449550206 <wangwenqi5261>
2020-01-12 21:30:21 +0800

    闪购的左栏实现

--- Market/models.py
+++ Market/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class AxfFoodType(models.Model):
+    typeid = models.CharField(max_length=32)
+    typename = models.CharField(max_length=64)
+    childtypenames = models.CharField(max_length=256)
+    typesort = models.IntegerField()
+
+    class Meta:
+        db_table='axf_foodtype'
+
--- Market/views.py
+++ Market/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+from Market.models import AxfFoodType
+
+
 def market(request):
-    return render(request,'axf/main/market/market.html')
+
+    foodtypes  = AxfFoodType.objects.all()
+
+    context = {
+        'foodtypes':foodtypes
+    }
+
+    return render(request,'axf/main/market/market.html',context=context)
