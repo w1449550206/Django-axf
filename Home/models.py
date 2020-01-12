697550125eba2cda4efdcb62ce66a27a019231f1
w1449550206 <wangwenqi5261>
2020-01-12 15:38:06 +0800

    完成首页

--- Home/models.py
+++ Home/models.py
@@ -49,3 +49,51 @@ class AxfMustBuy(models.Model):
 
     class Meta:
         db_table = 'axf_mustbuy'
+
+
+# mainshow
+
+# INSERT INTO axf_mainshow
+# (id, img, name, trackid, categoryid, brandname,
+# img1, childcid1, productid1, longname1, price1, marketprice1,
+# img2, childcid2, productid2, longname2, price2, marketprice2,
+# img3, childcid3, productid3, longname3, price3, marketprice3)
+# VALUES (1, '/media/images/ad01.jpg', '优选水果', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
+
+class AxfMainShow(models.Model):
+    img = models.CharField(max_length=128)
+    name = models.CharField(max_length=64)
+    trackid = models.IntegerField()
+    categoryid = models.IntegerField()
+    brandname = models.CharField(max_length=128)
+
+    img1=models.CharField(max_length=128)
+    childcid1= models.IntegerField()
+    productid1= models.IntegerField()
+    longname1=models.CharField(max_length=128)
+    price1=models.FloatField()
+    marketprice1=models.FloatField()
+
+    img2=models.CharField(max_length=128)
+    childcid2= models.IntegerField()
+    productid2= models.IntegerField()
+    longname2=models.CharField(max_length=128)
+    price2=models.FloatField()
+    marketprice2=models.FloatField()
+
+    img3=models.CharField(max_length=128)
+    childcid3= models.IntegerField()
+    productid3= models.IntegerField()
+    longname3=models.CharField(max_length=128)
+    price3=models.FloatField()
+    marketprice3=models.FloatField()
+
+    class Meta:
+        db_table = 'axf_mainshow'
+
+# INSERT INTO axf_mainshow (id, img, name, trackid, categoryid, brandname, img1, childcid1, productid1, longname1, price1, marketprice1, img2, childcid2, productid2, longname2, price2, marketprice2, img3, childcid3, productid3, longname3, price3, marketprice3) VALUES (1, '/media/images/ad01.jpg', '优选水果', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
+# INSERT INTO axf_mainshow (id, img, name, trackid, categoryid, brandname, img1, childcid1, productid1, longname1, price1, marketprice1, img2, childcid2, productid2, longname2, price2, marketprice2, img3, childcid3, productid3, longname3, price3, marketprice3) VALUES (2, '/media/images/ad02.jpg', '牛奶面包', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
+# INSERT INTO axf_mainshow (id, img, name, trackid, categoryid, brandname, img1, childcid1, productid1, longname1, price1, marketprice1, img2, childcid2, productid2, longname2, price2, marketprice2, img3, childcid3, productid3, longname3, price3, marketprice3) VALUES (3, '/media/images/ad01.jpg', '卤味熟食', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
+# INSERT INTO axf_mainshow (id, img, name, trackid, categoryid, brandname, img1, childcid1, productid1, longname1, price1, marketprice1, img2, childcid2, productid2, longname2, price2, marketprice2, img3, childcid3, productid3, longname3, price3, marketprice3) VALUES (4, '/media/images/ad02.jpg', '饮料酒水', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
+# INSERT INTO axf_mainshow (id, img, name, trackid, categoryid, brandname, img1, childcid1, productid1, longname1, price1, marketprice1, img2, childcid2, productid2, longname2, price2, marketprice2, img3, childcid3, productid3, longname3, price3, marketprice3) VALUES (5, '/media/images/ad01.jpg', '零食大趴', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
+# INSERT INTO axf_mainshow (id, img, name, trackid, categoryid, brandname, img1, childcid1, productid1, longname1, price1, marketprice1, img2, childcid2, productid2, longname2, price2, marketprice2, img3, childcid3, productid3, longname3, price3, marketprice3) VALUES (6, '/media/images/ad02.jpg', '方便速食', 21782, 103532, '爱鲜蜂', '/media/images/goods002.jpg', 103533, 118824, '爱鲜蜂·特小凤西瓜1.5-2.5kg/粒', 25.8, 25.8, '/media/images/goods003.jpg', 103534, 116950, '蜂觅·越南直采红心火龙果350-450g/盒', 15.3, 15.8, '/media/images/goods010.jpg', 103533, 118826, '爱鲜蜂·海南千禧果400-450g/盒', 9.9, 13.8);
--- Home/views.py
+++ Home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from Home.models import AxfWheel, AxfNav, AxfMustBuy
+from Home.models import AxfWheel, AxfNav, AxfMustBuy, AxfMainShow
 
 
 def home(request):
@@ -9,5 +9,6 @@ def home(request):
     wheels  = AxfWheel.objects.all()
     navs = AxfNav.objects.all()
     mustbuys = AxfMustBuy.objects.all()
+    mainshows = AxfMainShow.objects.all()
 
     return render(request,'axf/main/home/home.html',context=locals())
