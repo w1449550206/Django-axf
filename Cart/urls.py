b9f97a717b84907a1e9489671e0b7680085a7c14
w1449550206 <wangwenqi5261>
2020-01-14 19:29:07 +0800

    增加购物车的子路由：1.减少购物车商品数量；2.改变购物车选中状态。

--- Cart/urls.py
+++ Cart/urls.py
@@ -7,4 +7,10 @@ urlpatterns=[
 
 #     添加到购物车
     url(r'^addToCart/',views.addToCart),
+
+#     减少购物车商品数量
+    url(r'^subToCart/',views.subToCart),
+
+#     改变购物车选中状态
+    url(r'^changeStatus/',views.changeStatus),
 ]
