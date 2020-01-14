e9c28fc3ab1b488853ac334a94d3442a8cbbd1eb
w1449550206 <wangwenqi5261>
2020-01-14 11:41:45 +0800

    购物车的模型

--- Cart/models.py
+++ Cart/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from Market.models import AxfGoods
+from User.models import AxfUser
+
+
+class AxfCart(models.Model):
+    c_user = models.ForeignKey(AxfUser)
+
+    c_goods = models.ForeignKey(AxfGoods)
+
+    c_is_select = models.BooleanField(default=True)
+
+    c_goods_num = models.IntegerField(default=1)
+
+
+    class Meta:
+        db_table = 'axf_cart'
--- Cart/views.py
+++ Cart/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+from django.urls import reverse
+
+
 def cart(request):
+
     return render(request,'axf/main/cart/cart.html')
