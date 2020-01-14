53b92203120c047e8ba4ce5d565925e7e3cf35f2
w1449550206 <wangwenqi5261>
2020-01-14 11:47:28 +0800

    更改了登录页面 包括加密密码 二维码的验证

---
+++ static/axf/user/js/login.js
@@ -0,0 +1,18 @@
+$(function () {
+    $('#changeimg').click(function () {
+        $(this).attr('src','/axfuser/get_code/?'+Math.random())
+    })
+
+
+    $('form').submit(function () {
+        var password = $('#password').val();
+
+        var secret_password = md5(password);
+
+        $('#password').val(secret_password);
+
+
+
+        return true;
+    })
+})
