eafd370fcc3af3464568896c72b83d07d569e14a
w1449550206 <wangwenqi5261>
2020-01-14 16:06:34 +0800

    添加购物车子路由：加商品到购物车

--- Cart/urls.py
+++ Cart/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from Cart import views
 
 urlpatterns=[
-    url(r'^cart/',views.cart,name='cart')
+    url(r'^cart/',views.cart,name='cart'),
+
+#     添加到购物车
+    url(r'^addToCart/',views.addToCart),
 ]
