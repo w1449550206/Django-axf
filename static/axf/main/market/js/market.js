72f76a03f292a36c9a2625055e604f6ff1821a20
w1449550206 <wangwenqi5261>
2020-01-14 16:10:03 +0800

    更改闪购的js比如一些点击事件：1.增加商品到购物车

--- static/axf/main/market/js/market.js
+++ static/axf/main/market/js/market.js
@@ -9,4 +9,58 @@ $(function(){
         $(this).find('span').toggleClass('glyphicon glyphicon-chevron-down glyphicon glyphicon-chevron-up');
         $('#sort_rule_container').toggle()
     })
+
+
+
+
+    // //自定义属性
+        // var goodsid = $button.attr('goodsid');
+        // //系统自带的
+        // var $class = $button.prop('class');
+        //
+        // alert(goodsid);
+        // alert($class);
+
+        // var $goodsid = $button.prop('goodsid');
+        //
+        // var $class = $button.attr('class');
+        //
+        // alert($goodsid);
+        // alert($class);
+
+    //    attr方法可以获取自定义属性 也可以获取标签自带的属性
+    //    prop方法可以获取标签自带的属性 而不可以获取自定义属性
+
+
+
+
+
+    $('.addToCard').click(function () {
+    //    获取goodsid
+        var $button = $(this);
+
+        var goodsid = $button.attr('goodsid');
+
+
+        $.get('/axfcart/addToCart/',
+              {'goodsid':goodsid},
+              function (data) {
+                  if(data['status'] == 200){
+                        $button.prev().html(data['c_goods_num']);
+                  }
+              }
+            )
+
+
+
+
+
+
+
+
+    })
+
+
+
+
 })
