4f5071c25d2f97d9495110070b86ef7c96177542
w1449550206 <wangwenqi5261>
2020-01-16 21:43:17 +0800

    中间件userid

--- middleware/middleware.py
+++ middleware/middleware.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.utils.deprecation import MiddlewareMixin
@@ -7,13 +8,38 @@ class LoginMiddle(MiddlewareMixin):
 
     def process_request(self,request):
 
-        LOGIN_HTML_RQQUEST=[
-            '/axfcart/cart/'
+        LOGIN_HTML_REQUEST=[
+            '/axfcart/cart/',
+            '/axfmine/mine/'
         ]
 
-        if request.path in LOGIN_HTML_RQQUEST:
+
+        LOGIN_JSON_REQUEST = [
+            '/axfcart/addToCart/'
+        ]
+
+
+
+        if request.path in LOGIN_HTML_REQUEST:
             user_id = request.session.get('user_id')
 
-            if not user_id:
+            if user_id:
+                # 自定义属性   给request对象自己设定一个属性值
+                # 然后可以通过request.user_id获取
+                request.user_id = user_id
+            else:
                 return redirect(reverse('axfuser:login'))
+
+        if request.path in LOGIN_JSON_REQUEST:
+            user_id = request.session.get('user_id')
+
+            if user_id:
+                request.user_id = user_id
+
+            else:
+                data = {
+                    'status':201
+                }
+
+                return JsonResponse(data=data)
 
